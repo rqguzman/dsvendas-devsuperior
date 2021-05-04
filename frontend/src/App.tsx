4813fbe1a9d5ab782b727e497fe7f9e593bda159
rqguzman <rafael.guzman@outlook.com>
2021-05-03 22:05:28 -0300

    :sparkles: feat(components): Add basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,17 @@
+import DataTable from "components/DataTable/DataTable";
+import Footer from "components/Footer/Footer";
+import Navbar from "components/Navbar/Navbar";
+
 function App() {
   return (
-    <div>
-      <h1 className="text-primary">Hello world!</h1>
-    </div>
+    <>
+      <Navbar />
+      <div className="container">
+        <h1 className="text-primary">Hello world!</h1>
+        <DataTable />
+      </div>
+      <Footer />
+    </>
   );
 }
 
---
+++ frontend/src/components/Footer/Footer.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">Developed by <a href="https://github.com/rqguzman" target="_blank" rel="noreferrer">Rafael Guzmán</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                Workshop promoted by DevSuperior School: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
