4dd3d23bb7abce513994738086ff4ab79b6fe83e
rqguzman <rafael.guzman@outlook.com>
2021-05-03 22:30:54 -0300

    :sparkles: feat(charts): Add static charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,6 @@
+import BarChart from "components/BarChart/BarChart";
 import DataTable from "components/DataTable/DataTable";
+import DonutChart from "components/DonutChart/DonutChart";
 import Footer from "components/Footer/Footer";
 import Navbar from "components/Navbar/Navbar";
 
@@ -7,7 +9,22 @@ function App() {
     <>
       <Navbar />
       <div className="container">
-        <h1 className="text-primary">Hello world!</h1>
+        <h1 className="text-primary py-3">Sales Dashboard</h1>
+        <div className="row px-3">
+          
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Success rate (%)</h5>
+            <BarChart />
+          </div>
+          
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Success rate (%)</h5>
+            <DonutChart />
+          </div>
+        </div>
+        <div className="py-3">
+            <h2 className="text-primary">All sales (%)</h2>
+        </div>
         <DataTable />
       </div>
       <Footer />
