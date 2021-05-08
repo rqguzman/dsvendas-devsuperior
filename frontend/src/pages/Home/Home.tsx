165b9a8a9d00262233e61daaf3e7e8ca275ee741
rqguzman <rafael.guzman@outlook.com>
2021-05-08 10:19:24 -0300

    :sparkles: feat(routes): Add Home view and navigation

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,10 +1,25 @@
+import Footer from 'components/Footer/Footer';
+import Navbar from 'components/Navbar/Navbar';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
     return (
-        <div>
-            <h1>Página Home</h1>
-        </div>
+        <>
+            <Navbar />
+            <div className="container">
+                <div className="jumbotron">
+                    <h1 className="display-4">DSVendas</h1>
+                    <p className="lead">Analyze your sales' performance through different perspectives</p>
+                    <hr />
+                    <p>This application displays a dashboard from data provided by a backend built with Spring Boot.</p>
+                    <Link to="/dashboard" className="btn btn-primary btn-lg">
+                        Dashboard
+                    </Link>
+                </div>
+            </div>
+            <Footer/>
+        </>
     );
 };
 
